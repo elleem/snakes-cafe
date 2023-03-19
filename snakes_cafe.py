14def7194ba779bb341b9ce04c0d03ece2bbae0b
Lauren <lauren.main28@gmail.com>
2023-03-18 19:08:16 -0700

    starts dictionary and print functions

---
+++ snakes_cafe.py
@@ -0,0 +1,26 @@
+
+my_dict = {}
+
+menu = {
+    "Wings": 0,
+    "Cookies": 0,
+    "Spring Rolls": 0,
+    "Salmon": 0,
+    "Steak": 0,
+    "Meat Tornado": 0,
+    "A Literal Garden": 0,
+    "Ice Cream": 0,
+    "Cake": 0,
+    "Pie": 0,
+    "Coffee": 0,
+    "Tea": 0,
+    "Unicorn Tears": 0
+}
+def welcome():
+    pass
+
+def order_invitation():
+    pass
+
+def menu():
+    pass
