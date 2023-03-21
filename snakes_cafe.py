019a4eae62e6c81dc09cf1ac0c28ef57a669cf72
Lauren <lauren.main28@gmail.com>
2023-03-20 21:10:02 -0700

    updates function to loop and allows counting and off menu orders

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,7 +1,7 @@
 
-my_dict = {}
+my_current_order = {}
 
-dict = {
+snakes_menu = {
     "Wings": 0,
     "Cookies": 0,
     "Spring Rolls": 0,
@@ -14,10 +14,12 @@ dict = {
     "Pie": 0,
     "Coffee": 0,
     "Tea": 0,
-    "Unicorn Tears": 0
+    "Unicorn Tears": 0,
 }
+
 def welcome():
-    print('''**************************************
+    print('''
+    *************************************
     **    Welcome to the Snakes Cafe!   **
     **    Please see our menu below.    **
     **
@@ -25,38 +27,78 @@ def welcome():
     **************************************''')
 
 
+def menu():
+    print('''
+    Appetizers
+    ----------
+    Wings
+    Cookies
+    Spring Rolls
+
+    Entrees
+    -------
+    Salmon
+    Steak
+    Meat Tornado
+    A Literal Garden
+
+    Desserts
+    --------
+    Ice Cream
+    Cake
+    Pie
+
+    Drinks
+    ------
+    Coffee
+    Tea
+    Unicorn Tears''')
+
 def order_invitation():
     print('''
     ***********************************
     ** What would you like to order? **
     ***********************************''')
+    users_order = input("> ")
+    if users_order not in snakes_menu:
+        print(f'''
+        Your {users_order} is not on the menu, but we can make an exception.''')
+    else:
+        print(f'''
+    1 order of {users_order} has been added to your meal''')
+    if users_order in my_current_order:
+        my_current_order[users_order] = + 1
+    else:
+        my_current_order[users_order] = 1
 
-def menu():
-    print('''Appetizers
-----------
-Wings
-Cookies
-Spring Rolls
+    ask_again = True
 
-Entrees
--------
-Salmon
-Steak
-Meat Tornado
-A Literal Garden
+    while ask_again:
+        print('''
+        What else can I prepare for you today?
+        Type quit to quit at any time.''')
 
-Desserts
---------
-Ice Cream
-Cake
-Pie
+        users_order = input("> ")
+        if users_order == "quit":
+            break
+        if users_order not in snakes_menu:
+            print(f'''
+            Your {users_order} is not on the menu, but we can make an exception.''')
+        if users_order in my_current_order:
+            my_current_order[users_order] += 1
+            print(f'''
+            You have added {my_current_order[users_order]} orders to your meal of {users_order}.''')
+        else:
+            my_current_order[users_order] = 1
+            print (f'''
+            You have added {users_order} to your meal.''')
 
-Drinks
-------
-Coffee
-Tea
-Unicorn Tears''')
+        print(f'''
+        Your meal consists of : {my_current_order}''')
 
 welcome()
 menu()
 order_invitation()
+
+
+
